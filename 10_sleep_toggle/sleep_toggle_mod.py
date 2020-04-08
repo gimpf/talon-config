3be7dd236966c0d6212fb4f73f109b018f8a83e8
Markus Grüneis <gimpf@gimpf.org>
2020-04-08 18:38:49 +0200

    fix settings query for noise enabled

--- 10_sleep_toggle/sleep_toggle_mod.py
+++ 10_sleep_toggle/sleep_toggle_mod.py
@@ -5,7 +5,7 @@ from talon import Module, Context
 
 
 mod = Module()
-mod.setting('noise_enabled', type='bool', desc='Enable callbacks to noise events')
+mod.setting('noise_enabled', type=bool, desc='Enable callbacks to noise events')
 mod.mode('coma', desc='deep sleep where even noises are not handled anymore')
 
 @mod.action_class
--- 21_mouse/mouse_noise_keys.py
+++ 21_mouse/mouse_noise_keys.py
@@ -3,12 +3,12 @@ from talon import noise, ctrl, settings
 
 def on_pop(active):
     # for pop active = False always
-    if settings.get('user.noise_enabled'):
+    if settings.get('user.noise_enabled') == 'True':
         ctrl.mouse_click(button=1)
 
 
 def on_hiss(active):
-    if settings.get('user.noise_enabled'):
+    if settings.get('user.noise_enabled') == 'True':
         ctrl.mouse_click(button=0, down=active, up=not active)
 
 
