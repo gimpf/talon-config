2739efa6039e6c1f7b2160325d0717dae59dfa6b
Markus Grüneis <gimpf@gimpf.org>
2020-04-29 18:50:32 +0200

    cleanup setup-microphone

--- utils/setup-microphone.sh
+++ utils/setup-microphone.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+# thanks to
+# https://askubuntu.com/questions/17791/can-i-downmix-stereo-audio-to-mono
+# https://dx9s.net/node/32
+
 master_input=$(pacmd list-sources | sed -nre 's/\s+name: <(alsa_input.*focusrite.*analog-stereo)>/\1/ip')
 pacmd unload-module module-remap-source
-# device.description=\"Left channel of Focusrite Scarlett Solo\"
+
 pacmd load-module module-remap-source source_name=scarlett-mono source_properties="device.description=\"Left\ channel\ of\ Focusrite\ Scarlett\ Solo\"" "master=${master_input}" master_channel_map=front-left channels=1 channel_map=mono
