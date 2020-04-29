75802cec1a6b1c3147e93694443bee30e55e649d
Markus Grüneis <gimpf@gimpf.org>
2020-04-29 19:26:07 +0200

    update custom vocabulary example
    stolen from beta again

--- 26_custom_vocabulary/word_map_mod.py
+++ 26_custom_vocabulary/word_map_mod.py
@@ -4,24 +4,32 @@ mod = Module()
 mod.list("vocabulary", "dictionary of words to replace w/other stuff")
 
 
-@mod.capture(rule='{self.vocabulary}')
-def mapped_word(m) -> str:
-    """word returns a single mapped word from the vocabulary as string"""
+@mod.capture(rule='{user.vocabulary}')
+def vocab(m) -> str:
+    """returns a single mapped word from the vocabulary as string"""
     return m.vocabulary
 
 
-@mod.capture(rule='(<user.mapped_word> | <phrase>)+')
+@mod.capture(rule='(<word> | <user.vocab>)')
+def word(m) -> str:
+    return str(m)
+
+
+@mod.capture(rule='(<phrase> | <user.vocab>)+')
 def text(m) -> str:
     return str(m)
 
 
-ctx = Context()
-ctx.lists['self.vocabulary'] = {
-    "coffee cup": "kafka"
+simple_words = [
+    'terraform',
+]
+
+complex_words = {
+    'coffee cup': 'Kafka',
 }
 
-# alternatively, w/o translation
-# ctx.lists['self.vocabulary'] = [
-#     "kafka",
-#     "varkelshorp"
-# ]
+vocabulary = {k: k for k in simple_words}
+vocabulary.update(complex_words)
+
+ctx = Context()
+ctx.lists['user.vocabulary'] = vocabulary
