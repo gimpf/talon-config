afdbfacbcef93449029b0b697097340f2985473f
Markus Grüneis <gimpf@gimpf.org>
2020-04-29 19:43:34 +0200

    ensure no remixing for microphone source
    should not happen, but in case some master source
    channel has a different name, the remix=no will
    help us to spot it instead of getting inferior
    or wrong audio

--- utils/setup-microphone.sh
+++ utils/setup-microphone.sh
@@ -7,4 +7,4 @@
 master_input=$(pacmd list-sources | sed -nre 's/\s+name: <(alsa_input.*focusrite.*analog-stereo)>/\1/ip')
 pacmd unload-module module-remap-source
 
-pacmd load-module module-remap-source source_name=scarlett-mono source_properties="device.description=\"Left\ channel\ of\ Focusrite\ Scarlett\ Solo\"" "master=${master_input}" master_channel_map=front-left channels=1 channel_map=mono
+pacmd load-module module-remap-source source_name=scarlett-mono source_properties="device.description=\"Left\ channel\ of\ Focusrite\ Scarlett\ Solo\"" "master=${master_input}" master_channel_map=front-left rate=44100 channels=1 channel_map=mono remix=no
